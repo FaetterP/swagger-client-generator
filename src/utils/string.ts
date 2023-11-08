d1f69fa48c3d034987dead73afebc2894ffe07db
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-11-08 14:17:18 +0300

    Исправлены функции со строками
    Функция camelize не работала со строками, содержащими дефис.
    Из-за того, что http://localhost:3000 не подходит под регулярное выражение, проверка на url проходит только по тому, начинается ли строка с http.

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,5 +1,5 @@
 export function camelize(str: string): string {
-  return str
+  return str.replaceAll("-","")
     .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
@@ -11,8 +11,5 @@ export function capitalize(str: string) {
 }
 
 export function isUrl(str: string): boolean {
-  const regex = new RegExp(
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-  );
-  return Boolean(str.match(regex));
+  return str.startsWith("http");
 }
